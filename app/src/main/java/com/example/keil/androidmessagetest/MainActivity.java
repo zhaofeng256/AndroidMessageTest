d7df191fe4d285fa374ef76303479d8c3e226ef2
zhaofeng <zhaofeng256@foxmail.com>
2018-01-07 17:30:52 +0800

    add thread
    Signed-off-by: zhaofeng <zhaofeng256@foxmail.com>

--- app/src/main/java/com/example/keil/androidmessagetest/MainActivity.java
+++ app/src/main/java/com/example/keil/androidmessagetest/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.keil.androidmessagetest;
 
+import android.provider.Telephony;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -12,7 +13,39 @@ public class MainActivity extends ActionBarActivity {
     private String TAG = "AndroidMessageTest";
     private Button mButton;
     private int ButtonCount = 0;
-
+    private Thread thread1;
+    private MyThread thread2;
+    private class MyRunnable implements Runnable {
+    @Override
+        public void run() {
+            int count = 0;
+            while(true) {
+                count++;
+                Log.d(TAG, "thread1 count = " + count );
+                try {
+                    Thread.sleep(2000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+    private class MyThread extends Thread {
+        @Override
+        public void run() {
+            super.run();
+            int count = 0;
+            while(true) {
+                count++;
+                Log.d(TAG, "thread2 count = " + count );
+                try {
+                    Thread.sleep(2000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,6 +57,12 @@ public class MainActivity extends ActionBarActivity {
                 Log.i(TAG, "ButtonCount = " + ButtonCount);
             }
         });
+
+        thread1 = new Thread(new MyRunnable(), "THREAD1");
+        thread1.start();
+
+        thread2 = new MyThread();
+        thread2.start();
     }
 
     @Override
